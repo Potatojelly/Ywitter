78087e9d017eaa01061bc8cd691749f8b27aa2e7
Yoon <actominal123@gmail.com>
2023-07-22 16:29:04 -0700

    Fix being unable to go to myTweet page

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/:username",
-        element: <MyTweets/>
+        element: <MyTweets tweetService={tweetService}/>
       },
     ]
   }
