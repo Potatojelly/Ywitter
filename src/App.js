cfe69ab585633110ef3b6f1edf9aace1819e46e9
Yoon <actominal123@gmail.com>
2023-07-21 19:08:47 -0700

    Implement Header UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 import {Outlet} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import Header from './components/Header/Header';
 
 function App() {
+
+  const username = "user1";
+
+  const onAllTweets = () => {
+    return <Navigate to="/"/>
+  };
+  
+  const onMyTweets = () => {
+    return <Navigate to={`/${username}`} />
+  };
+
+  const onLogout = () => {
+    if(window.confirm("Do you want to log out?")) {
+      return <Navigate to="/"/>
+    }
+  }
   return (
-    <>
-      <Header/>
+    <div className="app">
+      <Header
+        username={username}
+        onLogout={onLogout}
+        onAllTweets={onAllTweets}
+        onMyTweets={onMyTweets}
+      />
       <Outlet/>
-    </>
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { memo } from 'react';
+import styles from './Header.module.css';
 
-export default function Header() {
+const Header = memo(({username, onLogout, onMyTweets, onAllTweets}) => {
     return (
-        <div>
-            Header
-        </div>
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src="./img/logo.png" alt="Twitter Logo" className={styles.logoImg} />
+                <h1 className={styles.logoName}>Ywitter</h1>
+                {username && <span className={styles.logoUser}>{username}</span>}
+            </div>
+            {username && (
+                <nav className={styles.menu}>
+                    <button onClick={onAllTweets}>All Tweets</button>
+                    <button onClick={onMyTweets}>My Tweets</button>
+                    <button onClick={onLogout} className={styles.menuItem}>Logout</button>
+                </nav>
+            )}
+        </header>
     );
-}
+}) 
 
+export default Header;
